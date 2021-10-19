2a883ad88d0e8bec658c942a566109f66cdd76aa
Jy0923 <pbj2y0923@naver.com>
2021-10-19 18:54:15 +0900

    train, inference에 normalize 추가

--- inference.py
+++ inference.py
@@ -63,6 +63,7 @@ if __name__ == "__main__":
     model = model.to(device)
 
     test_transform = A.Compose([
+                                A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
                                 ToTensorV2()
                                ])
 
--- train.py
+++ train.py
@@ -131,9 +131,11 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     train_transform = A.Compose([
+                                 A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
                                  ToTensorV2()
                                 ])
     val_transform = A.Compose([
+                               A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
                                ToTensorV2()
                               ])
 
