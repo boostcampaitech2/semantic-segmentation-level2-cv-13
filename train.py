942f79e0330205e38f22597699debb395a6bc0e8
YoojLee <daebaq27@gmail.com>
2021-10-25 06:57:40 +0000

    saving model checkpoints and the corresponding configuration

--- train.py
+++ train.py
@@ -1,14 +1,17 @@
+from collections import namedtuple
 import os
+import glob
+import json
+import re
+from importlib import import_module
+from pathlib import Path
+
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from utils import label_accuracy_score, add_hist, fix_seed, arg_parse
 from dataset import *
 import torch
 from torch.utils.data import DataLoader
 from torch.cuda.amp import autocast, GradScaler
-
-import json
-from collections import namedtuple
-from importlib import import_module
 
 # randomness control
 import random
@@ -35,15 +38,55 @@ category_dicts = {k:v for k,v in enumerate(category_names)}
 
 cur_date = datetime.today().strftime("%Sy%m%d")
 
+def increment_path(path, exist_ok=False):
+    """
+    Automatically increment path
+    
+    Args:
+        path (str or pathlib.Path): f"{saved_dir}/{wandb_run_name}"
+        exist_ok (bool): whether to increment path (increment path if False)
+
+    """
+
+    path = Path(path)
+    if (path.exists() and exist_ok) or (not path.exists()):
+        return str(path)
+    else:
+        dirs = glob.glob(f"{path}*")
+        matches = [re.search(rf"%s(\d+)" % path.stem, d) for d in dirs] 
+        # path.stem은 그 path에서 파일 이름에서 확장자빼고 가져옴.
+        i = [int(m.groups()[0]) for m in matches if m]
+        n = max(i) + 1 if i else 2
+        return f"{path}{n}"
+
+def save_checkpoint(epoch, model, loss, miou, optimizer, saved_dir, file_name, scheduler = None):
+    check_point = {'epoch': epoch,
+                    'net': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': loss,
+                    'miou': miou,
+                    }
+    if scheduler:
+        check_point['scheduler_state_dict'] = scheduler.state_dict()
+    output_path = os.path.join(saved_dir, file_name)
+    torch.save(check_point, output_path)
+
+def load_checkpoint(checkpoint_path, model, optimizer, scheduler):
+    # load model if resume_from is set
+    checkpoint = torch.load(checkpoint_path)
+    model.load_state_dict(checkpoint['net'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    if scheduler:
+        scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+    start_epoch = checkpoint['epoch']
+    start_loss = checkpoint['loss']
+    prv_best_miou = checkpoint(['miou'])
+
+    return model, optimizer, scheduler, start_epoch, start_loss, prv_best_miou
+
 
 def collate_fn(batch):
     return tuple(zip(*batch))
-
-def save_model(model, saved_dir, file_name):
-    check_point = {'net': model.state_dict()}
-    output_path = os.path.join(saved_dir, file_name)
-    torch.save(model, output_path)
-
 
 def validation(epoch, num_epochs, model, data_loader, criterion, device):
     model.eval()
@@ -117,17 +160,25 @@ def validation(epoch, num_epochs, model, data_loader, criterion, device):
         
     return avrg_loss, mIoU
 
-def train(num_epochs, model, train_loader, val_loader, criterion, optimizer, saved_dir, val_every, save_mode, device, scheduler = None, fp16 = False):
+def train(num_epochs, model, train_loader, val_loader, criterion, optimizer, 
+          saved_dir, val_every, save_mode, resume_from, checkpoint_path, 
+          device, scheduler = None, fp16 = False):
+
     print(f'Start training..')
+    start_epoch = 0
     n_class = 11
     best_loss = 9999999
     best_miou = 0
+    saved_dir = increment_path(saved_dir)
+
+    if resume_from:
+        model, optimizer, scheduler, start_epoch, best_loss, best_miou = load_checkpoint(checkpoint_path, model, optimizer, scheduler)
     
     if fp16:
         print("Mixed precision is applied")
         scaler = GradScaler()
 
-    for epoch in range(num_epochs):
+    for epoch in range(start_epoch, num_epochs):
         model.train()
 
         running_loss = None
@@ -198,17 +249,18 @@ def train(num_epochs, model, train_loader, val_loader, criterion, optimizer, sav
                     #save_dir = os.path.dirname(saved_dir)
                     if not os.path.exists(saved_dir):
                         os.makedirs(saved_dir)
-                    save_model(model, saved_dir, file_name=f"{model.model_name}_{best_loss}_{cur_date}.pt")
+                    save_checkpoint(epoch, model, best_loss, best_miou, optimizer, saved_dir, file_name=f"{model.model_name}_{round(best_loss,3)}_{cur_date}.pt")
                     
             else: # miou 기준 모델 저장
                 if miou > best_miou:
                     print(f"Best performance at epoch: {epoch + 1}")
                     print(f"Save model in {saved_dir}")
                     best_miou = miou
+                    best_loss = avrg_loss # best miou일 때도 loss tracking 수행 후 checkpoint에 저장
                     #save_dir = os.path.dirname(saved_dir)
                     if not os.path.exists(saved_dir):
                         os.makedirs(saved_dir)
-                    save_model(model, saved_dir, file_name=f"{model.model_name}_{best_miou}_{cur_date}.pt")
+                    save_checkpoint(epoch, model, best_loss, best_miou, optimizer, saved_dir, file_name=f"{model.model_name}_{round(best_miou, 3)}_{cur_date}.pt")
             
             # lr 조정
             if scheduler:
@@ -240,16 +292,19 @@ def main():
     # fix seed
     fix_seed(cfgs.seed)
     
-    ## wandb logging init
+    # wandb logging init
     wandb.init(project=cfgs.wandb_prj_name, name=cfgs.wandb_run_name, entity="cval_seg")
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+    # augmentation
     train_augmentation_module = getattr(import_module("augmentation"), cfgs.train_augmentation)
     train_augmentation = train_augmentation_module().transform
+
     val_augmentation_module = getattr(import_module("augmentation"), cfgs.val_augmentation)
     val_augmentation = val_augmentation_module().transform
 
+    # dataset & data loader
     train_dataset_module = getattr(import_module("dataset"), cfgs.train_dataset.name)
     train_dataset = train_dataset_module(cfgs.data_root, cfgs.train_json_path, **cfgs.train_dataset.args._asdict(), transform = train_augmentation)
     train_dataloader = DataLoader(train_dataset, **cfgs.train_dataloader.args._asdict(), collate_fn=collate_fn)
@@ -258,9 +313,12 @@ def main():
     val_dataset = val_dataset_module(cfgs.data_root, cfgs.val_json_path, **cfgs.val_dataset.args._asdict(), transform = val_augmentation)
     val_dataloader = DataLoader(val_dataset, **cfgs.val_dataloader.args._asdict(), collate_fn = collate_fn)
 
+
+    # model
     model_module = getattr(import_module("model"), cfgs.model.name)
     model = model_module(**cfgs.model.args._asdict()).to(device)
-    
+
+    # criterion
     if hasattr(import_module("criterions"), cfgs.criterion.name):
         criterion_module = getattr(import_module("criterions"), cfgs.criterion.name)
     else:
@@ -268,6 +326,7 @@ def main():
     
     criterion = criterion_module()
 
+    # optimizer
     if hasattr(import_module("optimizers"), cfgs.optimizer.name):
         optimizer_module = getattr(import_module("optimizers"), cfgs.optimizer.name)
     else:
@@ -275,6 +334,8 @@ def main():
 
     optimizer = optimizer_module(model.parameters(), **cfgs.optimizer.args._asdict())
 
+
+    # scheduler
     try:
         if hasattr(import_module("scheduler"), cfgs.scheduler.name):
             scheduler_module = getattr(import_module("scheduler"), cfgs.scheduler.name)
@@ -285,10 +346,34 @@ def main():
     except AttributeError :
             print('There is no Scheduler!')
             scheduler = None
+    
+    # get a path to save checkpoints and config
+    saved_dir = increment_path(f"{cfgs.saved_dir}/{cfgs.wandb_run_name}")
+    
+    # save a config.json before training
+    with open(f"{saved_dir}/config.json", "wb") as f:
+        json.dumps(cfgs, f)
 
-    train(cfgs.num_epochs, model, train_dataloader, val_dataloader, criterion, optimizer, cfgs.saved_dir, cfgs.val_every, cfgs.save_mode, device, scheduler, cfgs.fp16)
+    # call train
+    train_args = {
+        'num_epochs': cfgs.num_epochs, 
+        'model': model, 
+        'train_loader': train_dataloader, 
+        'val_loader': val_dataloader, 
+        'criterion': criterion, 
+        'optimizer': optimizer, 
+        'saved_dir': saved_dir, 
+        'val_every': cfgs.val_every, 
+        'save_mode': cfgs.save_mode, 
+        'resume_from': cfgs.resume_from, 
+        'checkpoint_path': cfgs.checkpoint_path,
+        'device': device,
+        'scheduler': scheduler,
+        'fp16': cfgs.fp16
+    }
 
-    wandb.run.finish()
+    train(**train_args)
+    wandb.run.finish() # close current wandb run session
 
 if __name__ == "__main__":
     main()
