452ad7d6d92d6c2afd473bdfb8cfbd95b2e3597d
tygu1004 <tygu1004@naver.com>
2021-10-28 08:34:58 +0000

    Add augmentation

--- augmentation.py
+++ augmentation.py
@@ -2,18 +2,70 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from copy_paste import CopyPaste
 
+
 class BaseAugmentation:
     def __init__(self, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
         self.transform = A.Compose([
             A.Normalize(mean=mean, std=std),
             ToTensorV2()
-            ])
+        ])
+
 
 class BaseCopyPasteAugmentation:
-    def __init__(self, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), data_root = "../input/data", json_dir = "./splited_json/train_split_0.json"):
+    def __init__(self, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), data_root="../input/data", json_dir="./splited_json/train_split_0.json"):
         self.transform = A.Compose([
-            CopyPaste(data_root = data_root, json_dir = json_dir, p = 1),
+            CopyPaste(data_root=data_root, json_dir=json_dir, p=1),
             A.Normalize(mean=mean, std=std),
             ToTensorV2()
-            ])
-            
+        ])
+
+
+class NoAugmentation:
+    def __init__(self):
+        self.transform = A.Compose([
+            ToTensorV2()
+        ])
+
+
+class JDJ_Augmentation:
+    def __init__(self, data_root, json_dir):
+        self.transform = A.Compose([
+            CopyPaste(data_root=data_root, json_dir=json_dir, p=1),
+            ##################
+            # shape          #
+            ##################
+            A.Rotate(),
+            A.HorizontalFlip(),
+            A.VerticalFlip(),
+            A.OpticalDistortion(),
+
+            ##################
+            # color or noise #
+            ##################
+            A.ColorJitter(),
+            A.Sharpen(p=0.2),
+            A.ISONoise(),
+            A.GlassBlur(),
+            A.OneOf([
+                A.RandomShadow(),
+                # 시간대에 따른 그림자
+                A.RandomSnow()
+                # 겨울철 눈 배경 사진
+            ], p=0.25),
+
+            A.OneOf([
+                A.RandomBrightnessContrast(),
+                A.RandomToneCurve()
+            ], p=0.25),
+            # 밤낮에 따른 배경 밝기
+
+            ##################
+            # delete         #
+            ##################
+            A.OneOf([
+                A.GridDropout(),
+                A.CoarseDropout()
+            ], p=0.25),
+
+            ToTensorV2()
+        ])
