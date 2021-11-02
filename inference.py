a56424e64cfed15e7763a679134c76157b5f8cde
tygu1004 <tygu1004@naver.com>
2021-11-02 05:04:28 +0000

    Add albumentation version
    Made additional albumentation resize version test function.

--- inference.py
+++ inference.py
@@ -1,7 +1,9 @@
+import numpy as np
 import pandas as pd
 import torch
 from torchvision import transforms
 from torch.utils.data import DataLoader
+from torchvision.transforms.functional import InterpolationMode
 from utils import fix_seed, arg_parse
 
 from importlib import import_module
@@ -10,15 +12,20 @@ import os
 import json
 from collections import namedtuple
 
+import albumentations as A
+from multiprocessing import Pool
+
+import time
+
 # tta
 import ttach as tta
 
 def collate_fn(batch):
     return tuple(zip(*batch))
 
-def test(model, test_loader, device):
+def test_torchvision(model, test_loader, device):
     size = 256
-    tf = transforms.Compose([transforms.Resize(size)])
+    tf = transforms.Compose([transforms.Resize(size, interpolation=InterpolationMode.BILINEAR, antialias=True)])
     print('Start prediction.')
 
     model.eval()
@@ -45,8 +52,49 @@ def test(model, test_loader, device):
     return file_names, preds_array
 
 
-if __name__ == "__main__":
+def albu_transform(oms):
+    tmp_mask = []
+    tf = A.Compose([A.Resize(256, 256)])
+    for mask in oms:
+        tmp_mask.append(tf(image=np.zeros_like(mask, dtype=np.uint8), mask=mask)['mask'].reshape(1, 256*256).astype(int))
+
+    return np.array(tmp_mask)
+
+def test_albumentation(model, test_loader, device, num_process=4):
+    print('Start prediction.')
+    model.eval()
+
+    size = 256    
+    file_name_list = []
+    preds_array = np.empty((0, size*size), dtype=np.compat.long)
     
+    pool = Pool(num_process)
+    work_list = []
+
+    with torch.no_grad():
+        for imgs, image_infos in tqdm(test_loader):
+            # inference (512 x 512)
+            imgs = torch.stack(imgs).to(device)
+            outs = model(imgs)['out']
+            oms = torch.argmax(outs.squeeze(), dim=1).detach().cpu().numpy()
+            
+            work_list.append(oms)
+            file_name_list.append([i['file_name'] for i in image_infos])
+
+    oms = pool.map(albu_transform, work_list)
+
+    for masks in oms:
+        preds_array = np.vstack((preds_array, np.squeeze(masks)))
+
+    
+    print("End prediction.")
+    file_names = [y for x in file_name_list for y in x]
+
+    return file_names, preds_array
+
+
+if __name__ == "__main__":
+    start = time.time()
     args = arg_parse()
 
     with open(args.cfg, 'r') as f:
@@ -84,7 +132,12 @@ if __name__ == "__main__":
 
     submission = pd.DataFrame(data={'image_id': [], 'PredictionString': []})
     # test set에 대한 prediction
-    file_names, preds = test(model, test_loader, device)
+    if cfgs.resize_type == "albumentations":
+        file_names, preds = test_albumentation(model, test_loader, device, cfgs.num_process)
+    elif cfgs.resize_type == "torchvision":
+        file_names, preds = test_torchvision(model, test_loader, device)
+    else:
+        raise Exception("cfgs.resize_type = (albumentation | torchvision)")
 
     # PredictionString 대입
     print("Generating Submission...")
@@ -97,3 +150,6 @@ if __name__ == "__main__":
         os.makedirs(cfgs.output_path)
 
     submission.to_csv(f"{cfgs.output_path}/submission_{output_name}.csv", index=False)
+
+    end = time.time()
+    print(f"{end - start:.5f} sec")
